c1146d6bcaef50dce2b9b7603f1eb765270588f0
Wendy C <chriscowendy@gmail.com>
2021-01-21 11:04:06 -0800

    Removed some extra props from WeatherSummary.js

--- src/WeatherSummary.js
+++ src/WeatherSummary.js
@@ -1,46 +1,22 @@
 import PropTypes from "prop-types";
 import Temperature from "./Temperature";
 
-const WeatherSummary = ({ location, dayOfWeek, weatherCondition, icon, units, currentTemp, highTemp,
-     lowTemp, percipitation, humidity, windSpeed }) => (
+const WeatherSummary = ({ dayOfWeek, icon, highTemp, lowTemp }) => (
     <div className="WeatherSummary">
-    <h1>{location}</h1>
       <p>{dayOfWeek}</p>
-      <p>{weatherCondition}</p>
-      <img src={`${icon}`} />
-     <h1 className="currentTemp">
-         <Temperature temp={currentTemp}/></h1>
-     <p>F | C</p>
+      <img src={`${icon}`} alt="weather icon" />
       <div className="WeatherSummary-temps">
-        <div>
-            High: <Temperature temp={highTemp} /> 
-            <br></br> 
-            Low: <Temperature temp={lowTemp} />
-            <br></br>
-            Percipitation: {percipitation}%
-            <br></br>
-            Humidity: {humidity}%
-            <br></br>
-            Wind: {windSpeed} mph
-            </div> 
-
- 
-      </div>
+            <Temperature temp={highTemp} /> 
+            <Temperature temp={lowTemp} />
+        </div> 
     </div>
   );
 
 WeatherSummary.propTypes = {
-    location: PropTypes.string.isRequired,
     dayOfWeek: PropTypes.string.isRequired,
-    weatherCondition: PropTypes.string.isRequired,
     icon: PropTypes.string.isRequired,
-    units: PropTypes.oneOf(['imperial', 'metric']),
-    currentTemp: PropTypes.number,
     highTemp: PropTypes.number.isRequired,
     lowTemp: PropTypes.number.isRequired,
-    percipitation: PropTypes.number.isRequired,
-    humidity: PropTypes.number.isRequired,
-    windSpeed: PropTypes.number.isRequired
 };
 
 WeatherSummary.defaultProps = {
